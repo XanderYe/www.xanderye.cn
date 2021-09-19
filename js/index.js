94401b6c2e976839d9adf07386678500f6463f81
XanderYe <XanderYe@outlook.com>
2021-09-19 15:55:37 +0800

    Initial Commit

---
+++ js/index.js
@@ -0,0 +1,60 @@
+new Vue({
+  el: "#app",
+  data() {
+    return {
+      bingImageUrl: null,
+      isInternet: true,
+      outerLinks: [
+        {
+          name: "我的博客",
+          image: "img/logo.png",
+          link: "https://www.xanderye.cn/"
+        },
+        {
+          name: "家庭影院",
+          image: "img/jellyfin.png",
+          link: "https://nas.xanderye.cn:8920/"
+        },
+        {
+          name: "工具小站",
+          image: "img/logo.png",
+          link: "http://tool.xanderye.cn/"
+        },
+      ],
+      innerLinks: [
+
+      ]
+    }
+  },
+  methods: {
+    bing() {
+      axios({
+        method: 'get',
+        url: 'http://nas.xanderye.cn:8081/tool/image/bingDaily'
+      }).then(function (res) {
+        if (res.data.code === 0) {
+          this.bingImageUrl = res.data.data;
+          sessionStorage.setItem("bingImageUrl", this.bingImageUrl);
+        } else {
+          this.bingImageUrl = "url ('img/WindowsTerminal.png')";
+        }
+        this.renderBackground();
+      });
+    },
+    renderBackground() {
+      document.body.style['background-image'] = "url('" + this.bingImageUrl + "')";
+    },
+  },
+  mounted() {
+
+  },
+  created() {
+    const bgUrl = sessionStorage.getItem("bingImageUrl");
+    if (bgUrl) {
+      this.bingImageUrl = bgUrl;
+      this.renderBackground();
+    } else {
+      this.bing();
+    }
+  }
+})
